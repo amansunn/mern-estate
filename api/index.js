b38652f4696ab6a940001e21c56a455a587be079
amansunn <lukamatic4ever@gmail.com>
2024-08-16 16:44:14 +0300

    create a middleware and a function to handle possible errors Updated

--- api/index.js
+++ api/index.js
@@ -26,4 +26,12 @@ console.log('server is runing on port 3000!!');
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
-
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server error';
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode,
+    });
+});
